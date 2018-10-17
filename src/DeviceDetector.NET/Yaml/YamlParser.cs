5cba80106903cf3c3531841fad03451549ad1800
Martin <prespic@gmail.com>
2018-10-17 12:27:46 +0200

    YamlParser takes yml files from embedded resources

--- src/DeviceDetector.NET/Yaml/YamlParser.cs
+++ src/DeviceDetector.NET/Yaml/YamlParser.cs
@@ -1,31 +1,38 @@
 using System.Collections;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using YamlDotNet.Core;
 using YamlDotNet.Core.Events;
 using YamlDotNet.Serialization;
 
 namespace DeviceDetectorNET.Yaml
 {
-    public class YamlParser<T> : IParser<T>
-        where T : class, IEnumerable// IParseLibrary
-    {
-        public T ParseFile(string file)
-        {
-            using (var r = new StreamReader(file))
-            {
-                var deserializer = new DeserializerBuilder().Build();
-                var parser = new YamlDotNet.Core.Parser(r);
+	public class YamlParser<T> : IParser<T>
+		where T : class, IEnumerable// IParseLibrary
+	{
+		public T ParseFile(string file)
+		{
+			var thisAssembly = Assembly.GetExecutingAssembly();
+			string filePath = "DeviceDetectorNET." + string.Join(".", file.Split('/')); 
+			using (var stream = thisAssembly.GetManifestResourceStream(filePath))
+			{
+				using (var r = new StreamReader(stream))
+				{
+					var deserializer = new DeserializerBuilder().Build();
+					var parser = new YamlDotNet.Core.Parser(r);
 
-                // Consume the stream start event "manually"
-                parser.Expect<StreamStart>();
+					// Consume the stream start event "manually"
+					parser.Expect<StreamStart>();
 
-                while (parser.Accept<DocumentStart>())
-                    // Deserialize the document
-                {
-                    return deserializer.Deserialize<T>(parser);
-                }
-                return null;
-            }
-        }
-    }
+					while (parser.Accept<DocumentStart>())
+					// Deserialize the document
+					{
+						return deserializer.Deserialize<T>(parser);
+					}
+					return null;
+				}
+			}
+		}
+	}
 }
